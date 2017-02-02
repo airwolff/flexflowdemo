fc56f057bd1ea6bbfe44b28e517bea0ebf161027
Andrew Wolff <andy.wolff@gmail.com>
2017-02-02 09:39:53 -0600

    ssl

--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -3,7 +3,7 @@ var poolConfig = {
   database: process.env.DATA_BASE,
   password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
   host: process.env.DATA_BASE_HOST, // Server hosting the postgres database
-  ssl: true,
+  //ssl: true,
   // port: 5432,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
